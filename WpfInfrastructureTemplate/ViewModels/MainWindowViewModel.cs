93e0e050c87120d4db652c57e19b8050606e24a2
Araik <iaraik99@gmail.com>
2022-06-06 21:40:27 +0900

    Добавьте файлы проекта.

---
+++ WpfInfrastructureTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfInfrastructureTemplate.ViewModels.Base;
+
+namespace WpfInfrastructureTemplate.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+        private string _title;
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get =>  _title;
+            set =>  Set(ref _title, value);
+        }
+        #endregion
+    }
+}
